b0b9e1a670387cd74ec726533153e0b05761f1bb
MarottiFrancisco <marottifranciscogabriel@gmail.com>
2023-10-28 19:43:40 -0300

    estado de completado true

--- controller/tareasController.js
+++ controller/tareasController.js
@@ -1,4 +1,28 @@
-import { tareas } from "../model/tareas.js";
+let tareas = [{
+    id: '1',
+    titulo: 'trabajar',
+    descripcion: 'horario de 9 a 18',
+    asignado: 'Andy',
+    completado: true,
+    prioridad: 'alta'
+},
+{
+    id: '2',
+    titulo: 'cocinar',
+    descripcion: 'milanesas',
+    asignado: 'Martin',
+    completado: false,
+    prioridad: 'baja'
+},
+{
+    id: '3',
+    titulo: 'estudiar',
+    descripcion: 'matematicas',
+    asignado: 'Facundo',
+    completado: true,
+    prioridad: 'alta'
+}]
+
 
 function getTareas(){
     return tareas
@@ -8,7 +32,7 @@ function getTareasById(id){
 }
 
 function createTarea(tarea){
-    const nuevaTarea = {id: Math.floor(Math.random() * Date.now()).toString(16), completado: false, ...tarea}
+    const nuevaTarea = {...tarea, id: Math.floor(Math.random() * Date.now()).toString(16) , completado: false}
     tareas.push(nuevaTarea)
     return tareas
 }
@@ -16,12 +40,11 @@ function createTarea(tarea){
 function completarTareaById(id){
     const nuevasTareas = tareas.map(tarea => {
         if (tarea.id === id) {
-            return {completado: true, ...tarea}
+            return { ...tarea, completado: true}
         }
         return tarea
     })
-    tareas.
-    tareas.push(...nuevasTareas)
+    tareas = nuevasTareas
     return tareas
 }
 
